cbb2e0e4a5f49e6e778394b8224c9bb3d84cf233
Ahmed Moutaz El Bakkali Telidi <ael-bakk@c2r9s6.42malaga.com>
2025-11-12 14:05:01 +0100

    añadir get_next_line y ft_putstr

--- minishell.c
+++ minishell.c
@@ -1,7 +1,18 @@
 #include <stdio.h>
+#include <unistd.h>
 
 int	main(void)
 {
 	printf("Hola Mundo!");
 	return (0);
+}
+
+void	ft_putstr(char *str)
+{
+	int i = 0;
+	while(str[i])
+	{
+		write(1, &str[i], 1);
+		i++;
+	}
 }
